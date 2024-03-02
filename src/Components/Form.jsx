5e580df11b24d04631a41423776bf1046bcf674e
Vinay Kumar <vinaykumarraju21@gmail.com>
2024-03-02 15:35:29 +0530

    static Home page done

---
+++ src/Components/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Form() {
+    return (
+        <>
+            <div>
+                <h1>This is Form</h1>
+            </div>
+        </>
+
+    )
+}
+
+export default Form
---
+++ src/Components/Recipe.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Recipe({ recipes }) {
+    return (
+        <>
+            <div className='bg-gray-200 flex m-2.5 '>
+                <img src={recipes.image} className='h-64 w-60 rounded-2xl mr-2' />
+
+                <div className='items-center'>
+                    <h2 className='pb-2 font-bold text-2xl'>{recipes.title}</h2>
+                    <p>{recipes.description}</p>
+                    <div className='pt-2.5 flex'>
+                        {
+                            Array(recipes.rating)
+                                .fill()
+                                .map((_) => (
+                                    <p>⭐</p>
+                                ))
+                        }
+                    </div>
+                </div>
+
+
+
+            </div>
+
+        </>
+    )
+}
+
+export default Recipe
